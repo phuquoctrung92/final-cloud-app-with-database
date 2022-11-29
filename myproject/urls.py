aebda1fc893fe2272b45e1b4833b784ba2e71f91
phuquoctrung <phuquoctrung92@gmail.com>
2022-11-29 07:59:58 -0500

    init commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""myproject URL Configurations
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
